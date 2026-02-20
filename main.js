8dd62911a5e293c230c22742968acf841785cdb3
Dhamotharan <mddhamotharan281@gmail.com>
2026-02-20 17:19:31 +0530

    feat:router structure

--- main.js
+++ main.js
@@ -5,6 +5,29 @@ const port=900;
 app.get("/",(req,res)=>{
     res.json({msg:"Hello students!..."});
 });
+
+//CRUD function of movies
+//c
+
+app.post("/movies",(req,res)=>{
+    res.json({msg:"Movie added successfully..."});
+});
+
+//r
+app.get("/movies",(req, res)=>{
+    res.json({msg:"All movies..."});
+});
+//u
+app.put("/movies/:id",(req,res)=>{
+    res.json({msg:"update"})
+})
+
+
+//d
+
+app.delete("/movies",(req,res)=>{
+    res.json({msg:"movie deleted"})
+})
 app.listen(port,
     ()=>
     console.log(`Server is running on port
